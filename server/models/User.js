8c70c912e0623aa28a0f319a6effdc1b60ccf8a3
Adarsh L K <erlkadarsh@gmail.com>
2025-06-11 00:30:01 +0530

    Add User Model

---
+++ server/models/User.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const UserSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  age: { type: Number },
+  gender: { type: String, enum: ['male', 'female', 'other'] },
+  medicalHistory: [String],
+  createdAt: { type: Date, default: Date.now },
+});
+
+// Hash password before saving
+UserSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Method to check password validity
+UserSchema.methods.comparePassword = async function(candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
+
+module.exports = mongoose.model('User', UserSchema);
